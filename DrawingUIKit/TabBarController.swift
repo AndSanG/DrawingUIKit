b973e5ff5ead9826878fc287c335787e63c2cac1
Andrés Sánchez <and_sg@outlook.com>
2025-09-17 15:46:11 -0500

    Create a project with UIKit and Storyboard. Each option is part of a TabBar that has Navigation, the main screen and a Detail. The storyboard has three options 	- UiKit SB that goes as far as possible only with SB Implemented 	- UIKit CD that avoid using SB - Pendig 	- SwiftUI SB that uses the UIHostingController in a segueAction more SB - pending 	- SwiftUI CD that extend the UIHostingController more code Add a TabBar controller but not used yet

---
+++ DrawingUIKit/TabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarController.swift
+//  DrawingUIKit
+//
+//  Created by Andres Sanchez on 15/9/25.
+//
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("TabBarController")
+    }
+}
